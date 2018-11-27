24a23a7341d06b1d3b7cffad353d859f8fa96a5e
Nick Hansen <nick.hansen13@gmail.com>
2018-11-27 14:18:16 -0600

    code for exercise

---
+++ main.js
@@ -0,0 +1,123 @@
+let students = [
+    {
+        name: "Chris Miller",
+        class: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        class: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        class: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        class: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        class: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        class: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        class: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        class: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        class: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        class: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        class: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        class: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+let containerRef = document.querySelector("#container");
+console.log (containerRef);
+
+const h1 = (title, style, color) => {
+    const h1Element = document.createElement('h1');
+    h1Element.textContent = title;
+    h1Element.classList.add(style);
+    h1Element.classList.add(color);
+    return h1Element;
+}
+
+const section = (title, style) => {
+    const sectionElement = document.createElement('section');
+    sectionElement.textContent = title;
+    sectionElement.classList.add(style);
+    return sectionElement;
+}
+
+const aside = (title, style) => {
+    const asideElement = document.createElement('aside');
+    asideElement.textContent = title;
+    asideElement.classList.add("bordered", "dashed", style);
+    return asideElement;
+}
+
+const student = (name, className, info) => {
+    const divElement = document.createElement('div');
+    divElement.appendChild(h1(name, "xx-large", "passing"));
+    divElement.appendChild(section(className, "section-padded"));
+    divElement.appendChild(aside(info, "pushRight"));
+    return divElement;
+}
+const studentFail = (name, className, info) => {
+    const divElement = document.createElement('div');
+    divElement.appendChild(h1(name, "xx-large", "failing"));
+    divElement.appendChild(section(className, "section-padded"));
+    divElement.appendChild(aside(info, "pushRight"));
+    return divElement;
+}
+for ( i = 0; i < students.length; i++) {
+    if (students[i].score >= 60) {
+        containerRef.appendChild(student(students[i].name, students[i].class, students[i].info));
+        } else {
+            containerRef.appendChild(studentFail(students[i].name, students[i].class, students[i].info));
+        }
+    }
+
+
